d6f28744dabbb9c488d333f6255f44315968b7a5
Portgas-D-Ace <dlapiashvili@deltasystems.ge>
2014-07-19 03:32:54 +0400

    Delegator Server
    Server for android application

---
+++ src/database/MySQLCommunicator.java
@@ -0,0 +1,9 @@
+package database;
+
+public class MySQLCommunicator {
+	
+	public static void main(String[] args) {
+		System.out.println(MySQLDBInfo.initConnectionTest());
+	}
+	
+}
---
+++ src/database/MySQLDBInfo.java
@@ -0,0 +1,81 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MySQLDBInfo {
+	//ბაზის პარამეტრები
+		public static final String MYSQL_USERNAME = "root";
+		public static final String MYSQL_PASSWORD = "Jomolungma8848";
+		public static final String MYSQL_DATABASE_HOST = "localhost";
+		public static final String MYSQL_DATABASE_PORT = "3306";
+		public static final String MYSQL_DATABASE_NAME = "delegator";
+		public static final String MYSQL_DATABASE_DRIVER = "com.mysql.jdbc.Driver";
+		private static Connection con;
+		
+		/**
+		 * Base Initialization
+		 * @return
+		 */
+		public static Connection initConnection(){
+			String url = "";
+			if(con==null){
+			        url = "jdbc:mysql://" + MYSQL_DATABASE_HOST + ":" + MYSQL_DATABASE_PORT + "/" + MYSQL_DATABASE_NAME;
+			        try {
+						Class.forName(MYSQL_DATABASE_DRIVER);
+						con = DriverManager.getConnection(url, MYSQL_USERNAME, MYSQL_PASSWORD);
+					} catch (ClassNotFoundException e) {
+						e.printStackTrace();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+			    	
+			}
+			return con;
+		}
+		
+		/**
+		 * Initializing connection for testing
+		 * @return String if connection is successful or not
+		 */
+	    public static String initConnectionTest(){
+	        String url = "";
+	        if(con==null){
+	            try {
+	                url = "jdbc:mysql://" + MYSQL_DATABASE_HOST + ":" + MYSQL_DATABASE_PORT + "/" + MYSQL_DATABASE_NAME;
+	                Class.forName(MYSQL_DATABASE_DRIVER);
+	                con = DriverManager.getConnection(url, MYSQL_USERNAME, MYSQL_PASSWORD);
+	            } catch (SQLException e) {
+	                e.printStackTrace();
+	                return e.getMessage();
+	            } catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	        }
+	        return "Connection Initialized!";
+	    }
+	 
+	    /**
+	     * Returns connection to the base
+	     * @return
+	     */
+	    public static Connection getConnection() {
+	        if (con == null) {
+	            initConnection();
+	        }
+	        return con;
+	    }
+	    
+		/**
+		 * Starts statement
+		 * @throws SQLException 
+		 */
+		public static Statement startUpStatement(Connection con) throws SQLException{
+			Statement st = con.createStatement();
+			st.executeQuery("USE " +MySQLDBInfo.MYSQL_DATABASE_NAME);
+			return st;
+		}
+}
